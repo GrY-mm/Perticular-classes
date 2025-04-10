9dc4c82789f7cf5b7677b3bde7d99237dafc384f
GrY-mm <xxknifedark@gmail.com>
2025-04-11 01:11:03 +0200

    feat: Agrega configuración de colección para el contenido educativo

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const subject = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        icon: z.string(),
+        location: z.object{(
+            granada: z.string(),
+        )}
+    }),
+})
+
+export const collections = { subject };
